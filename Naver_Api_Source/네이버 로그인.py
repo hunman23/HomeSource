24d4f549c8503afdd764463a730bb875dc4775d2
sunghun hwang <hunman23@gmail.com>
2017-08-03 21:16:14 +0900

    Delete 네이버 로그인.py

--- Naver_Api_Source/네이버 로그인.py
+++
@@ -1,9 +0,0 @@
-from selenium import webdriver
-
-driver = webdriver.Chrome(r'C:\Users\HunPC\Downloads\chromedriver_win32\chromedriver.exe')
-driver.get('https://nid.naver.com/nidlogin.login')
-driver.find_element_by_name('id').send_keys('')
-driver.find_element_by_name('pw').send_keys('')
-
-driver.find_element_by_xpath('//*[@id="frmNIDLogin"]/fieldset/input').click()
-
